3ea50a841b3740df37da6fb45bd24c621070ca29
DooaAnsari <dansari@digital-dividend.com>
2020-05-02 06:28:06 +0500

    initialising

---
+++ src/Login.js
@@ -0,0 +1,19 @@
+//Step 1
+import React from "react";
+import { View} from "react-native";
+
+
+//Step 2 and Step 3
+class Login extends React.Component {
+    
+    //Step 4
+    render() {
+       return <View></View>      
+    }
+
+
+}
+//Step 5
+export default Login
+
+
